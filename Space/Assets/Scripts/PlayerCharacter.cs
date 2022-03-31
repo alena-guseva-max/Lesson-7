c56b89c58912490501f8057ade9c4a5e6a0f0ce8
alena-guseva-max <guseva2201@gmail.com>
2022-03-31 20:25:21 +0300

    second

---
+++ Space/Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //библиотека для работы со сценами
+
+public class PlayerCharacter : MonoBehaviour
+{
+    private int _health = 500;
+    private int maxHealth = 1000;
+
+    public void Hurt(int damage)
+    {
+        _health -= damage;//уменьшение здоровья игрока
+        if (_health <= 0)
+        {
+            SceneManager.LoadScene("SampleScene");//Перезагрузить сцену SamleScene
+        }
+        Debug.Log("Health: " + _health);
+    }
+}
